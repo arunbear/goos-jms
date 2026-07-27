29287313688f7fdb924f03ef4ad8947a435e90f1
Arun Prasaad <arunbear@cpan.org>
2026-07-27 15:10:02 +0100

    Add initial project files

---
+++ src/main/java/org/example/sniper/GoosJmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.sniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoosJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoosJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sniper/GoosJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoosJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
